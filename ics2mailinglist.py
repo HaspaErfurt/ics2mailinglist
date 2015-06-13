9701ce7623748b4cd77b3b2db9dbe2404b1f8f97
Maik Kulbe <info@linux-web-development.de>
2015-06-13 18:56:01 +0200

    PEP8 changes

--- ics2mailinglist.py
+++ ics2mailinglist.py
@@ -7,19 +7,19 @@ Sends calendar dates to a mailing list
 
 """
 from config import *
-from icalendar      import Calendar, Event
+from icalendar import Calendar
 from icalendar.prop import vDDDTypes
-from datetime       import *
-from textwrap       import wrap
-from urllib         import urlopen
-from pytz           import utc, timezone
+from datetime import *
+from textwrap import wrap
+from urllib import urlopen
+from pytz import utc, timezone
 import smtplib
 
-f     = urlopen(cal_file)
-cal   = Calendar.from_ical(f.read())
-mail  = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" 
-         % (mail_from, mail_to, mail_subject))
-now   = datetime.now(utc).replace(hour=0, minute=0, second=0, microsecond=0)
+f = urlopen(cal_file)
+cal = Calendar.from_ical(f.read())
+mail = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
+        % (mail_from, mail_to, mail_subject))
+now = datetime.now(utc).replace(hour=0, minute=0, second=0, microsecond=0)
 nweek = now + timedelta(weeks=1)
 timezoneEF = timezone('Europe/Berlin')
 fmt = "%d.%m.%Y, %H:%M"
@@ -29,28 +29,33 @@ for ev in cal.walk():
         # Check the start and end dates of the event
         if len(str(vDDDTypes.from_ical(ev.get('dtstart'))).split(' ')) > 1:
             start = vDDDTypes.from_ical(ev.get('dtstart')).replace(tzinfo=utc)
-            end   = vDDDTypes.from_ical(ev.get('dtend')).replace(tzinfo=utc)
+            end = vDDDTypes.from_ical(ev.get('dtend')).replace(tzinfo=utc)
             if start < now or start > nweek:
                 continue
         else:
             start = vDDDTypes.from_ical(ev.get('dtstart'))
-            end   = vDDDTypes.from_ical(ev.get('dtend'))
+            end = vDDDTypes.from_ical(ev.get('dtend'))
             if start < now.date() or start > nweek.date():
                 continue
 
         mail += 'Betreff:      ' + ev.get('summary').encode('utf-8') + "\n"
-        mail += 'Start:        ' + str(ev.get('dtstart').dt.astimezone(timezoneEF).strftime(fmt)) + "\n"
-        mail += 'Ende:         ' + str(ev.get('dtend').dt.astimezone(timezoneEF).strftime(fmt)) + "\n"
+        mail += 'Start:        ' + str(ev.get('dtstart').dt.
+                                       astimezone(timezoneEF).
+                                       strftime(fmt)) + "\n"
+        mail += 'Ende:         ' + str(ev.get('dtend').dt.
+                                       astimezone(timezoneEF).
+                                       strftime(fmt)) + "\n"
 
         if ev.get('location'):
             mail += 'Ort:          ' + ev.get('location').encode('utf-8') + "\n"
         if ev.get('description'):
             mail += "\nBeschreibung: "
-            mail += "\n".join(wrap(ev.get('description').encode('utf-8'), 65, subsequent_indent=(' ' * 14)))
+            mail += "\n".join(wrap(ev.get('description').encode('utf-8'), 65,
+                                   subsequent_indent=(' ' * 14)))
         mail += "\n\n"
 
 # Send mail via SMTP
-server = smtplib.SMTP_SSL(host = smtp_server)
+server = smtplib.SMTP_SSL(host=smtp_server)
 server.login(smtp_user, smtp_pass)
 server.sendmail(mail_from, mail_to, mail)
 
